a22cc17bd24deff615d53c75f625eefa6768aac5
Muchina-Mungai <muchinahmungai@gmail.com>
2025-07-22 18:07:47 +0300

    Reset of version for exxtension bundle

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import logging
 
 app = func.FunctionApp()
+@app.function_name(name="openai_app")
 @app.route(route="openai_function",auth_level=func.Auth_Level.ANONYMOUS)
 def openai_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
